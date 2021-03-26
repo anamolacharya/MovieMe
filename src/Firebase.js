8d9512dd0dea3692344455b2d53f65fdbe10b87a
anamolacharya <anamol333@gmailcom>
2021-03-25 21:34:45 -0500

    Created the firebase.js file and added instruction

---
+++ src/Firebase.js
@@ -0,0 +1,4 @@
+/* to get firebase in the terminal, first  install firebase using npm.
+Then, run 'firebase login', then it will ask user to allow firebase to collect CLI usage and 
+error reporting information. 
+*/
