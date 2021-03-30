19e739ed47fce7d8dc3b808e08d8dab4b33c322d
anamolacharya <anamol333@gmailcom>
2021-03-30 15:14:59 -0500

    added instructio for git pull

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,6 @@
-/* to get firebase in the terminal, first  install firebase using npm.
+/*
+ReadMe:
+to get firebase in the terminal, first  install firebase using npm.
 Then, run 'firebase login', then it will ask user to allow firebase to collect CLI usage and 
 error reporting information. 
 Run firebase init
@@ -7,5 +9,12 @@ Look for the exisitng project and we need to make sure run 'build' in this quest
 Then hit yes in this question Configure as a single-page app (rewrite all urls to /index.html)?
 hit npm run build
 then, hit firebase deploy
+
+
+
+Git Pull:
+git stash
+git clean -d -f
+then try git pull origin master
 
 */
