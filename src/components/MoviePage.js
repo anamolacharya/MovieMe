8c5cb45d3ad57be0d1904c6e458d0b82ad750f44
anamolacharya <anamol333@gmailcom>
2021-05-01 14:10:42 -0500

    Added streaming options

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,67 +1,145 @@
-import axios from '../axios';
-import React , {useState, useEffect} from 'react';
+import axios from "../axios";
+import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
 import "../css/Footer.css";
-import Row from "./Row"
+import Row from "./Row";
 import {
-    BrowserRouter as Router,
-    Link,
-    Route,
-    useParams
-} from 'react-router-dom'
+  BrowserRouter as Router,
+  Link,
+  Route,
+  useParams,
+} from "react-router-dom";
 import { checkPropTypes } from "prop-types";
-
-
 
 function MoviePage() {
   const movieID = useParams().movieID;
   const [movie, setMovie] = useState([]);
   const [image, setImage] = useState([]);
+  const [watchProvider, setWatchProvider] = useState([]);
+  const [imdb, setImdb] = useState([]);
   const [loaded, setLoaded] = useState(false);
-  const fetchURL = 'https://api.themoviedb.org/3/movie/' + movieID +'?api_key=6bc6187ce75ef23b68c83c1f02848597&language=en-US';
-  const fetchImageURL = 'https://api.themoviedb.org/3/movie/' + movieID +'/images?api_key=6bc6187ce75ef23b68c83c1f02848597';
+  const fetchURL =
+    "https://api.themoviedb.org/3/movie/" +
+    movieID +
+    "?api_key=6bc6187ce75ef23b68c83c1f02848597&language=en-US";
+  const fetchImageURL =
+    "https://api.themoviedb.org/3/movie/" +
+    movieID +
+    "/images?api_key=6bc6187ce75ef23b68c83c1f02848597";
+  const fetchWatchProvider =
+    "https://api.themoviedb.org/3/movie/" +
+    movieID +
+    "/watch/providers?api_key=6bc6187ce75ef23b68c83c1f02848597";
 
-//This useEffect helps to get the data from the API
-useEffect(() =>{
+  const API = "343f6c9f5dmsh4866102b7bb0306p113e69jsn7eefe752434e";
+  const Host = "movie-database-imdb-alternative.p.rapidapi.com";
 
-    //if the bracket is blanck [], it will run once and do not run again 
-    async function fetchData(){
-        const request = await axios.get(fetchURL);
-        const imageRequest = await axios.get(fetchImageURL);
-        setMovie(request.data);
-        setImage(imageRequest.data)
-        console.log(imageRequest.data);
-        setLoaded(true);
-        //console.log("ImageURL: " + image.backdrops[0])
-        return request;
+  // const fetchIMDB =
+  //   `https://movie-database-imdb-alternative.p.rapidapi.com/?i=${movie.imdb_id}` +
+  //   "&rapidapi-key=" +
+  //   API +
+  //   "&rapidapi-host=" +
+  //   Host;
+  // const fetchIMDB = {
+  //   method: "GET",
+  //   url: "https://movie-database-imdb-alternative.p.rapidapi.com/",
+  //   params: { i: "tt3717532", r: "json" },
+  //   headers: {
+  //     "x-rapidapi-key": "343f6c9f5dmsh4866102b7bb0306p113e69jsn7eefe752434e",
+  //     "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+  //   },
+  // };
+
+  const fetchIMDB = {
+    method: "GET",
+    url: "https://movie-database-imdb-alternative.p.rapidapi.com/",
+    params: { i: `${movie.imdb_id}`, r: "json" },
+    headers: {
+      "x-rapidapi-key": "343f6c9f5dmsh4866102b7bb0306p113e69jsn7eefe752434e",
+      "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(fetchIMDB)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  //This useEffect helps to get the data from the API
+  useEffect(() => {
+    //if the bracket is blanck [], it will run once and do not run again
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      const imageRequest = await axios.get(fetchImageURL);
+      const streamProviderRequest = await axios.get(fetchWatchProvider);
+      //const IMDBRequest = await axios.get(fetchIMDB);
+
+      // console.log(streamProviderRequest);
+
+      setMovie(request.data);
+      setImage(imageRequest.data);
+      setWatchProvider(streamProviderRequest.data);
+      setImdb(fetchIMDB.data);
+      //console.log(imageRequest.data);
+      setLoaded(true);
+      //console.log("ImageURL: " + image.backdrops[0])
+      return request;
     }
     fetchData();
-},[fetchURL]);
-if (!loaded) {
-  return <h2 style={{ paddingTop: "85px", display: "flex", alignItems: "center", justifyContent: "center"}}>Loading</h2>;
-} else {
-    return (
-        <div>
-          <img
-            className="background_image"
-            src="https://user-images.githubusercontent.com/55467685/115977981-35903580-a542-11eb-8cb7-06ddc3b5335e.png"
-            alt="bg-image"
-          />
-          <div>
-        <h1 className="about-us-header"> {movie.title}</h1>
-        <h1 className="overview_text"> {movie.overview}</h1>
-        <h1 className="stream_text">Streaming Link</h1>
-        <h1 className="IMDB_text">IMDB Rating</h1>
-        <h1 className="favorite_text">Favorite this: </h1>
-        <img className = "movie_image" src={image.backdrops[0] ? "https://image.tmdb.org/t/p/w500" + image.backdrops[0].file_path : ''} />
-      </div>
-          <div className="footer_position">
-            <Footer />
-          </div>
-        </div>
+  }, [fetchURL]);
 
-        );
-    }
-    }
+  if (!loaded) {
+    return (
+      <h2
+        style={{
+          paddingTop: "85px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Loading
+      </h2>
+    );
+  } else {
+    return (
+      <div>
+        <img
+          className="background_image"
+          src="https://user-images.githubusercontent.com/55467685/115977981-35903580-a542-11eb-8cb7-06ddc3b5335e.png"
+          alt="bg-image"
+        />
+        <div>
+          <h1 className="about-us-header"> {movie.title}</h1>
+          <h1 className="overview_text"> {movie.homepage}</h1>
+          <h1 className="stream_text">Streaming Options:</h1>
+          <p className="stream_provider">
+            {watchProvider.results.US.rent.map((x) => {
+              return x.provider_name;
+            })}
+          </p>
+          <h1 className="IMDB_text">IMDB Rating : {imdb.Year}</h1>
+          <h1 className="favorite_text">Favorite this: </h1>
+          <img
+            className="movie_image"
+            src={
+              image.backdrops[0]
+                ? "https://image.tmdb.org/t/p/w500" +
+                  image.backdrops[0].file_path
+                : ""
+            }
+          />
+        </div>
+        <div className="footer_position">
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default MoviePage;
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,30 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useState, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import "../css/SignUp.css";
-
-
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         Movie Me
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -32,25 +30,22 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 }));
-
-
-
 
 export default function SignUp() {
   const classes = useStyles();
@@ -82,15 +77,20 @@ export default function SignUp() {
     setError(false);
     setSignupStatus(false);
 
-    let data = JSON.stringify({ name: firstName, email: email, password: password, password_confirmation: password });
+    let data = JSON.stringify({
+      name: firstName,
+      email: email,
+      password: password,
+      password_confirmation: password,
+    });
 
     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: data
-  };
-  fetch('http://localhost:8000/api/signup', requestOptions)
-      .then(async response => {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: data,
+    };
+    fetch("http://localhost:8000/api/signup", requestOptions)
+      .then(async (response) => {
         const responseData = await response.json();
 
         if (!response.ok) {
@@ -100,118 +100,142 @@ export default function SignUp() {
         } else {
           setSignupStatus(true);
         }
-
-
       })
-      .catch(error => {
+      .catch((error) => {
         setError(error.toString());
-        console.error('There was an error!', error);
-    });
-
-
-  }
+        console.error("There was an error!", error);
+      });
+  };
 
   return (
-
     <div className="back">
-    {error ? <h2 style={{ paddingTop: "85px", color: "red", display: "flex", alignItems: "center", justifyContent: "center"}}>Error {error}, please check your information and try again. If you already have an account please sign in</h2>: <h2></h2>}
-    {signupStatus ? <h2 style={{ paddingTop: "85px", color: "green", display: "flex", alignItems: "center", justifyContent: "center"}}>Success! Please Sign in.</h2>: <h2></h2>}
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                value={firstName}
-                onChange={handleFirstNameChange}
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
+      {error ? (
+        <h2
+          style={{
+            paddingTop: "85px",
+            color: "red",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Error {error}, please check your information and try again. If you
+          already have an account please sign in
+        </h2>
+      ) : (
+        <h2></h2>
+      )}
+      {signupStatus ? (
+        <h2
+          style={{
+            paddingTop: "85px",
+            color: "green",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Success! Please Sign in.
+        </h2>
+      ) : (
+        <h2></h2>
+      )}
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  variant="outlined"
+                  value={firstName}
+                  onChange={handleFirstNameChange}
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="outlined"
+                  value={lastName}
+                  onChange={handleLastNameChange}
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  value={email}
+                  onChange={handleEmailChange}
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive new movie notifications, marketing promotions and updates via email."
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                value={lastName}
-                onChange={handleLastNameChange}
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                value={email}
-                onChange={handleEmailChange}
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                value={password}
-                onChange={handlePasswordChange}
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive new movie notifications, marketing promotions and updates via email."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
+          </form>
+        </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
     </div>
   );
-
-  
 }
