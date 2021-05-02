3289a082c0654f356f6db6ef061dd3282a5e6a0b
shresthashashank <shashank1611.sp@gmail.com>
2021-05-02 18:50:19 -0500

    Search functionality implemented

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -31,23 +31,22 @@ function MoviePage() {
     "https://api.themoviedb.org/3/movie/" +
     movieID +
     "/watch/providers?api_key=6bc6187ce75ef23b68c83c1f02848597";
-    let fetchIMDB = "";
+  let fetchIMDB = "";
 
   //This useEffect helps to get the data from the API
   useEffect(() => {
     //if the bracket is blanck [], it will run once and do not run again
     async function fetchData() {
-      const request = await axios.get(fetchURL).then(response => {
+      const request = await axios.get(fetchURL).then((response) => {
         fetchIMDB =
-        "http://www.omdbapi.com/?i=" + response.data.imdb_id + "&apikey=513d661";
+          "http://www.omdbapi.com/?i=" +
+          response.data.imdb_id +
+          "&apikey=513d661";
         setMovie(response.data);
-      }
-
-      );
+      });
       const imageRequest = await axios.get(fetchImageURL);
       const streamProviderRequest = await axios.get(fetchWatchProvider);
       const IMDBRequest = await axios.get(fetchIMDB);
-
 
       setImage(imageRequest.data);
       setWatchProvider(streamProviderRequest.data);
@@ -64,7 +63,7 @@ function MoviePage() {
     e.preventDefault();
 
     let data = JSON.stringify({
-      movieID: movieID
+      movieID: movieID,
     });
 
     const requestOptions = {
@@ -73,7 +72,10 @@ function MoviePage() {
       body: data,
     };
 
-      fetch("http://localhost:8000/moviepage/"+movieID+"/favorite", requestOptions)
+    fetch(
+      "http://localhost:8000/moviepage/" + movieID + "/favorite",
+      requestOptions
+    )
       .then(async (response) => {
         const responseData = await response.json();
         console.log(responseData);
@@ -87,9 +89,7 @@ function MoviePage() {
       .catch((error) => {
         console.error("There was an error!", error);
       });
-  
-      
-  }
+  };
 
   if (!loaded) {
     return (
@@ -104,7 +104,8 @@ function MoviePage() {
         Loading
       </h2>
     );
-  } else {console.log(movie.imdb_id);
+  } else {
+    console.log(movie.imdb_id);
     return (
       <div>
         <img
@@ -115,23 +116,30 @@ function MoviePage() {
         <div>
           <h1 className="about-us-header"> {movie.title}</h1>
           <h1 className="overview_text"> {movie.overview}</h1>
-          <h1 className="stream_text">Streaming Options in USA:</h1>
-          <p className="stream_provider">
-            {watchProvider.results.US.rent
-              ? watchProvider.results.US.rent.map((x) => {
-                  return x.provider_name;
-                })
-              : watchProvider.results.US.flatrate.map((y) => {
-                  return y.provider_name;
-                })}
-          </p>
+          <div>
+            <h1 className="stream_text">Streaming Options in USA:</h1>
+            <p className="stream_provider">
+              {watchProvider.results.US.rent
+                ? watchProvider.results.US.rent.map((x) => {
+                    return x.provider_name;
+                  })
+                : watchProvider.results.US.flatrate.map((y) => {
+                    return y.provider_name;
+                  })}
+            </p>
+          </div>
+
           {/* <h1 className="IMDB_text">IMDB Ratingss: {imdb.imdbRating}</h1> */}
           <h1 className="IMDB_text">
             IMDB Ratings:
-            <a href={"https://imdb.com/title/"+movie.imdb_id}>{imdb.imdbRating}</a>
+            <a href={"https://imdb.com/title/" + movie.imdb_id}>
+              {imdb.imdbRating}
+            </a>
           </h1>
 
-          <h1 className="favorite_text">Favorite this: <button onClick={handleFavorite}>Favorite</button></h1>
+          <h1 className="favorite_text">
+            Favorite this: <button onClick={handleFavorite}>Favorite</button>
+          </h1>
           <img
             className="movie_image"
             src={
